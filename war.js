a77c15ae531ed7877e3b7cee3a403907569a96d9
DallasLind <mcgraw.dallas@gmail.com>
2019-10-10 21:49:32 -0400

    reinitialized files

---
+++ war.js
@@ -0,0 +1,45 @@
+// Establishing the cards players utilize
+let suits = ["clubs", "hearts", "diamonds", "spades"];
+let cardNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, "J", "Q", "K", "A"];
+// 14 = Ace/ 13 = King/ 12 = Queen/ 11 = Joker
+
+//Fill out user attributes here + if statements when they win
+let player1 = {
+
+}
+
+let player2 = {
+
+}
+
+//Shuffle cards 
+function shuffle(array){
+	let currentIndex.array.length;
+	let randomIndex;
+	let temporaryValue;
+	while(currentIndex > 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+	}
+	return array;
+}
+
+//Split off into two decks randomly
+function dealCards(){
+	for(var cardInDeck){
+		playerOne.push(card)
+	};
+}
+
+//Have the cards face-down, unknown to the other player
+
+//Use splice() to add or remove from arrays to each other
+
+//Actual ruleset of if/else statements depending on which card is played
+
+
+//Alert for "win" when win state is achieved
+//Print who won, the round itself, and how many cards they have now
+
+
+//Bonus if time to spare: work on saving scores throughout matches
